0f995e3f96316c0b389f6fe5ddea2a662159427a
iwenLjr <1816807688@qq.com>
2021-03-02 14:26:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iwen/broadcast/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.iwen.broadcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
